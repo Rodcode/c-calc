acd212a705b1bb7148cee2c71366c041646c3bb9
crodriguez1a <carlosrodriguez@carloss-mbp.lan>
2019-02-20 15:56:08 -0500

    Add some scaffolding, potential tools

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # c-calc
 A simple calculator
+
+**Testing**
+
+https://github.com/ThrowTheSwitch/Unity
+
+**Dependency Manager**
+
+https://github.com/clibs/clib
+
+**Some tools**
+http://www.throwtheswitch.org/home
--- calc.c
+++ calc.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
-int main()
+#include <stdlib.h>
+
+int sum_array(int arr[]); // TODO understand these declarations
+
+int add(int numbers[])
 {
-   printf("Let's Go!");
+    return sum_array(numbers);
+}
+
+int sum_array(int arr[])
+{
+   int i, sum=0;
+   int n = sizeof(&arr)/sizeof(int) + 1; // TODO better understand the ampersand here
+
+   for (i=0; i<n; i++)
+   {
+	 sum = sum + arr[i];
+   }
+   return(sum);
+}
+
+int main() //(int argc, char *argv[])
+{
+   printf("Let's Go!\n");
+
+   // char *ptr;
+   // printf("%ld", strtol(argv[1], &ptr, 10)); // TODO accept arguments, cast to integers
+
+   int inputs[] = {2, 3, 4}; // TODO not global
+   int answer = add(inputs);
+   printf("2 + 3 + 4 = %d\n", answer);
+
    return 0;
 }
